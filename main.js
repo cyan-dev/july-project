7bb8fc2065f379b3750fccb922a9aa21c70fa6c9
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-24 15:49:19 +0200

    Edition du main.js (ça marche pas)

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+
+// Variable globale correspondant à la fenpetre principale
+let mainWindow
+
+function createWindow() {
+    // Création de la fenêtre
+    mainWindow = new BrowserWindow({width: 1600, height: 900})
+
+    // Assignation du index.html à la fenêtre
+    mainWindow.loadFile('index.html')
+
+    // Déférencement de la mainWindow lors de la fermeture.
+    mainWindow.on('closed', () => {
+        mainWindows = null
+    })
+}
+
+// Quand l'app est completement chargé on crée la fenêtre
+app.on('ready', createWindow)
+
+
+// La suite concerne les Mac OS et leurs gestion particulière de la 
+// fermeture des fenêtres
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow()
+    }
+})
