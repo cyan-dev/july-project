00a96bc46bafe00c4f6a204ae6a7e75164078c18
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-25 17:57:01 +0200

    Ajout du compteur a l'écran et création de la fonction qui incrémente la money

--- js/game.js
+++ js/game.js
@@ -6,10 +6,21 @@ let enemy = document.createElement('img')
 enemy.src = './media/img/murloc.gif'
 enemy.alt = 'murloc des profondeurs'
 
+let moneyDisplay = document.createElement('p')
+moneyDisplay.textContent = money + ' pc'
+mainDiv.appendChild(moneyDisplay)
+
+mainDiv.appendChild(moneyDisplay)
+
+function addMoney(money, moneyDisplay) {
+    money += 1
+    moneyDisplay.textContent = money + ' pc'
+
+    return money
+}
+
 mainDiv.appendChild(enemy)
 
 enemy.addEventListener('click', () => {
-    money += 1
-    console.log('money : ' + money)
+    money = addMoney(money, moneyDisplay)
 })
-
