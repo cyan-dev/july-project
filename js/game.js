aac1e707420447209dc71067ce08e56f272c85e7
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-25 17:36:44 +0200

    Ajout de l'évenement qui surveille le clic sur le Murloc et qui ajoute 1 pièce de cuivre.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,15 @@
+let money = 0
+
 mainDiv = document.getElementById('maindiv')
 
 let enemy = document.createElement('img')
-enemy.src = './media/img/murloc.png'
+enemy.src = './media/img/murloc.gif'
 enemy.alt = 'murloc des profondeurs'
 
 mainDiv.appendChild(enemy)
+
+enemy.addEventListener('click', () => {
+    money += 1
+    console.log('money : ' + money)
+})
+
