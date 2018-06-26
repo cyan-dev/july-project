f2adf46a31d8aa798e631c14865d1fb6b4437c9c
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-26 10:30:24 +0200

    waitTime now display %

--- js/time.js
+++ js/time.js
@@ -6,8 +6,9 @@ function waitTime(time /*temps en millisecondes*/) {
     var e = new Event('wakeUp');
 
     while(Date.now() < start + time) {
-
+        console.log(Math.floor((Date.now() - start) / time * 100) + '%')
     }
     
     window.dispatchEvent(e);
+
 }
