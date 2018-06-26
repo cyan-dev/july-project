14201810b55a068c6af7d54f7816949e0b8e09c9
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-26 15:45:26 +0200

    ¤ Suppression de time.js ¤ utilisation de la fonction setTimeout() à la place de waitTime() => LE CLIC MARCHE TOUTES LES SECONDES ¤ Modification de la liste de script d'index.html en conséquence

--- js/game.js
+++ js/game.js
@@ -4,13 +4,14 @@ let money = 0
 
 let mainDiv = document.getElementById('maindiv');
 
-let enemy = document.createElement('img');
-enemy.src = './media/img/murloc.gif';
-enemy.alt = 'murloc';
-
 let moneyDisplay = document.createElement('p');
 moneyDisplay.textContent = money + ' pc';
 mainDiv.appendChild(moneyDisplay);
+
+let enemy = document.createElement('img');
+enemy.src = './media/img/murloc.gif';
+enemy.alt = 'murloc';
+mainDiv.appendChild(enemy);
 
 function addMoney(money, moneyDisplay) {
     money += 1;
@@ -19,17 +20,16 @@ function addMoney(money, moneyDisplay) {
     return money;
 }
 
-mainDiv.appendChild(enemy);
+let test = true
 
-function enemyEvent() {
-    enemy.addEventListener('click', () => {
-        let w = false;
-        w = waitTime(1000);
-        money = addMoney(money, moneyDisplay);
-        if(w) {
-            enemyEvent();
-        }
-    }, {once:true});
+function listenClickOnMurlock() {
+    enemy.onclick = () => {
+        enemy.onclick = null
+        setTimeout(() => {
+            money = addMoney(money, moneyDisplay);
+            listenClickOnMurlock();
+        }, 1000);
+    }
 }
 
-enemyEvent();
+listenClickOnMurlock()
