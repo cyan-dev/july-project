6255fbcee63ccaba23e7870dd60fd13189466a33
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-26 10:07:46 +0200

    fix waitTime

--- js/time.js
+++ js/time.js
@@ -1,21 +1,15 @@
 "use strict";
 
 function waitTime(time /*temps en millisecondes*/) {
-    let d = new Date();
-<<<<<<< HEAD
-    let limit = d.now() + time;
-
-    var e = new Event('wakeUp');
-=======
-    let limit = d.getTime() + time;
+    let limit = Date.now() + time;
 
     var e = new Event('wakeUp');
 
-    while(d.getTime() < limit) {
->>>>>>> 305c279e1469b4362c364bd1b9a9c5b69d35d5c8
+    while(Date.now() < limit) {
 
-    while(d.now() < limit) {
     }
     
     window.dispatchEvent(e);
 }
+
+waitTime(500);
