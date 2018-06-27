02ea32c4cdfb9ce71fe03c2c969176e98791f807
Dylan Caron <dcaron81.dc@gmail.com>
2018-06-27 15:26:10 +0200

    ¤ Ajout de points virgules manquants ¤ Ajout de commentary de base pour les fonctions ¤ suppression de la variable "test" qui ne sers plus à rien.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let money = 0
+let money = 0;
 
 let mainDiv = document.getElementById('maindiv');
 
@@ -9,21 +9,27 @@ moneyDisplay.textContent = money + ' pc';
 mainDiv.appendChild(moneyDisplay);
 
 let enemy = document.createElement('img');
-enemy.id = 'mlurg'
+enemy.id = 'mlurg';
 enemy.src = './media/img/murloc.gif';
 enemy.alt = 'murloc';
 mainDiv.appendChild(enemy);
 
 function addMoney(money, moneyDisplay) {
+    /*
+    Cette fonction ajoute 1 pièce de cuivre au compteur de monnaie et gère 
+    l'affichage du compteur de pièce en conséquences.
+    */
     money += 1;
     moneyDisplay.textContent = money + ' pc';
 
     return money;
 }
 
-let test = true
-
 function suffer() {
+    /*
+    Cette fonction gère l'animation du Murlock lorsqu'il prend un coup 
+    (lorsqu'on clique dessus)
+    */
     enemy.style.transform = 'translate(-55%, -50%)';
     setTimeout(() => {
         enemy.style.transform = 'translate(-45%, -50%)';
@@ -33,15 +39,18 @@ function suffer() {
     },100);
 }
 
-function listenClickOnMurlock() {
+function listenClickOnMurloc() {
+    /*
+    Cette fonction gère l'écoute de l'événement de click sur le Murloc
+    */
     enemy.onclick = () => {
-        enemy.onclick = null
-        suffer()
+        enemy.onclick = null;
+        suffer();
         setTimeout(() => {
             money = addMoney(money, moneyDisplay);
-            listenClickOnMurlock();
+            listenClickOnMurloc();
         }, 1000);
     }
 }
 
-listenClickOnMurlock()
+listenClickOnMurloc();
