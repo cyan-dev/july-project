e22bf987e7c9ebe1c81ee245e3a0956cf3de98d0
cesclass <cyril.esclassan@gmail.com>
2018-06-28 15:16:13 +0200

    bg site WoW + test abo event clock pour métiers

--- js/test.js
+++ js/test.js
@@ -1,5 +1,31 @@
 "use strict";
 
+var Job = new Object();
+Job.initQTE = 20;
+Job.workQTE = Job.initQTE;
+Job.worked = 0;
+
+Job.work = (self) => {
+    if(self.workQTE === 1) {
+        self.workQTE = self.initQTE;
+        self.worked += 1;
+        return 1;
+    } else {
+        self.workQTE -= 1;
+        return 0;
+    }
+}
+
+Job.showWork = (self) => {
+    console.debug(self.worked);
+}
+
+Job.active = (self) => {
+    if(self.work(self) == 1) {
+        self.showWork(self);
+    }
+}
+
 window.addEventListener("clock", function(e) {
-    console.log(e.type);
+    Job.active(Job);
 });
