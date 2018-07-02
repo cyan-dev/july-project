fc497a70e714170e8f5d9afdcdaf80455e4ca8b1
cesclass <cyril.esclassan@gmail.com>
2018-07-02 16:53:59 +0200

    WORK WORK WORK WORK WORK

--- js/game.js
+++ js/game.js
@@ -3,12 +3,76 @@
 var game = Object();
 
 game.money = 0;
-
-//game.cuisine = new Job();
-//game.peche = new Job();
-//game.archeologie = new Job();
-//game.secourisme = new Job();
+game.floatMoney = 0;
+game.cycle = 15;
 
 game.addMoney = () => {
     game.money += 1;
 }
+
+/*  Mutateurs des metiers  */
+game.lvlupJob = function(self) {
+    self.lvlJob += 1;
+    game.setProduction(self);
+}
+
+game.lvlupBuild = function(self) {
+    self.lvlBuild += 1;
+    game.setProduction(self);
+}
+
+game.setProduction = function(self) {
+    self.production = (
+        self.lvlJob * Math.pow(2, self.lvlBuild) / game.cycle
+    );
+}
+
+
+/*  Instanciation des metiers  */
+game.jobs = new Array()
+
+game.alchemy = new Job('alchimie');
+    game.jobs.push('alchemy');
+
+game.archeology = new Job('archeologie');
+    game.jobs.push('archeology');
+
+game.blacksmithing = new Job('forge');
+    game.jobs.push('blacksmithing');
+
+game.cooking = new Job('cuisine');
+    game.jobs.push('cooking');
+
+game.enchanting = new Job('enchantement');
+    game.jobs.push('enchanting');
+
+game.engineering = new Job('ingenierie');
+    game.jobs.push('engineering');
+
+game.firstAid = new Job('secourisme');
+    game.jobs.push('firstAid');
+
+game.fishing = new Job('peche');
+    game.jobs.push('fishing');
+
+game.herbalism = new Job('herboristerie');
+    game.jobs.push('herbalism');
+
+game.inscription = new Job('caligraphie');
+    game.jobs.push('inscription');
+
+game.jewelcrafting = new Job('joillerie');
+    game.jobs.push('jewelcrafting');
+
+game.leatherworking = new Job('travail du cuir');
+    game.jobs.push('leatherworking');
+
+game.mining = new Job('minage');
+    game.jobs.push('mining');
+
+game.skinning = new Job('depecage');
+    game.jobs.push('skinning');
+
+game.tailoring = new Job('couture');
+    game.jobs.push('tailoring');
+
--- js/job.js
+++ js/job.js
@@ -7,33 +7,22 @@ function Job(name) {
     this.lvlJob = 0;    // niveau de metier
     this.lvlBuild = 0;  // niveau du batiment
 
-    this.production = lvlJob * Math.pow(2, this.lvlBuild);
-
+    this.production = null;
+    game.setProduction(this);
 
     /*  Methodes  */
     this.work = function() {
-        console.debug('work ' + this.name);
+        game.floatMoney += this.production * 100;
     }
 
-    this.lvlupJob = function() {
-        this.lvlJob += 1;
-    }
-
-    this.lvlupBuild = function() {
-        this.lvlBuild += 1;
-    }
 }
 
-/*
+
 window.addEventListener('clock', () => {
-    r.forEach((job) => {
-        window[job].work();
+    game.jobs.forEach((job) => {
+        game[job].work();
     })
+    game.money += Math.floor(game.floatMoney / 100);
+    game.floatMoney %= 100;
+    ui.displayMoney(game.money);
 });
-
-var r = new Array();
-
-var a = new Job('a'); r.push('a');
-var b = new Job('b'); r.push('b');
-var c = new Job('c'); r.push('c');
-*/
